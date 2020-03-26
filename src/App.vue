2dceb7c03d93ad629f42331b469b8c1e89082548
Ray <mnzone.cn@gmail.com>
2020-03-26 10:15:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <button @click="click">Button</button>
+    <p v-if="toggle">Toggle</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data () {
+    return {
+      toggle: false
+    }
+  },
+  methods: {
+    click () {
+      this.toggle = !this.toggle
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
